f5467ed039f21bc53f5659c2c35e4acda66bff43
acthiagoflexa <87791447+acthiagoflexa@users.noreply.github.com>
2021-08-31 00:17:18 -0300

    Adicionar arquivos de projeto.

---
+++ DadosPeca.cs
@@ -0,0 +1,14 @@
+﻿namespace interface_01
+{
+    public class DadosPeca
+    {
+        public DadosPeca()
+        {
+
+        }
+
+        public int codigoPeca { get; set; }
+        public string descricaoPeca { get; set; }
+        public string dimensaoPeca { get; set; }
+    }
+}
---
+++ Peca.cs
@@ -0,0 +1,15 @@
+﻿namespace interface_01
+{
+    public class Peca
+    {
+        public Peca()
+        {
+
+        }
+
+        public int codigo { get; set; }
+        public string descricao { get; set; }
+        public double ladoA { get; set; }
+        public double ladoB { get; set; }
+    }
+}
